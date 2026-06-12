5090ab000b4b16a2b3bbcd4ab671b4e4a25e88ea
safarhmn <149854254+safarahman9@users.noreply.github.com>
2026-06-12 06:13:35 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+const CACHE = "cyber-v2";
+const FILES = ["./index.html", "./manifest.json", "./icon.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});
